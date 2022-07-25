ab18242b00270174a5093a9ed4d9ed188ac7c6f6
zzhhbs <908924736@qq.com>
2022-07-25 14:15:08 +0800

    1112

---
+++ Studyjava/day08/src/com/banyuan/java1/TestPerson.java
@@ -0,0 +1,17 @@
+package com.banyuan.java1;
+
+public class TestPerson {
+    public static void main(String[] args) {
+
+        Person p1= new Person("蔡徐坤",35,"中国");
+        Person p2= new Person("成龙",65,"中国");
+        Person p3= new Person("王心凌",45,"加拿大");
+
+
+
+        p1.show();
+        p2.show();
+        p3.show();
+
+    }
+}
